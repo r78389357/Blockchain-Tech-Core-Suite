1785eefaaeb3c45c9d8ca2ccc758a7bda491d987
r78389357 <r78389357@cdzhongguan.com>
2026-04-02 15:59:19 +0800

    Create erc20_mintable_token.sol

---
+++ erc20_mintable_token.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20";
+import "@openzeppelin/contracts/access/Ownable";
+
+contract MintableERC20Token is ERC20, Ownable {
+    string public constant TOKEN_NAME = "GlobalChain Token";
+    string public constant TOKEN_SYMBOL = "GCT";
+    uint8 public constant TOKEN_DECIMALS = 18;
+
+    constructor() ERC20(TOKEN_NAME, TOKEN_SYMBOL) Ownable(msg.sender) {}
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount * (10 ** uint256(TOKEN_DECIMALS)));
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+
+    function burnFrom(address account, uint256 amount) external {
+        _spendAllowance(account, msg.sender, amount);
+        _burn(account, amount);
+    }
+}
